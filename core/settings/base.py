2fc858b585edebeb68c601170ef687a4fe6710e1
sobirjonovme <huntsman6004@gmail.com>
2023-03-19 16:56:27 +0500

    feat: TimeStampedModel and CustomUser models created

--- core/settings/base.py
+++ core/settings/base.py
@@ -43,7 +43,7 @@ CUSTOM_APPS = [
     "apps.library",
     "apps.main",
     "apps.news",
-    "apps.user",
+    "apps.users",
 ]
 
 THIRD_PARTY_APPS = [
@@ -52,6 +52,7 @@ THIRD_PARTY_APPS = [
     # "rest_framework_simplejwt",
     "ckeditor",
     "ckeditor_uploader",
+    'phonenumber_field',
     # "django_filters",
     # "rest_framework.authtoken",
     # "dj_rest_auth",
@@ -149,3 +150,5 @@ MEDIA_ROOT = BASE_DIR / "media"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = 'users.CustomUser'

8e557c8ce8817350b1a3e410d415d177970a0748
AzizJohn <azizjon1708@gmail.com>
2023-03-23 12:51:17 +0500

    fixing conflicts

--- apps/v1.py
+++ apps/v1.py
@@ -1,12 +1,12 @@
-from django.urls import path, include
+from django.urls import include, path
 
 urlpatterns = [
-    path('cart/', include('apps.cart.urls')),
-    path('common/', include('apps.common.urls')),
-    path('course/', include('apps.course.urls')),
-    path('feedback/', include('apps.feedback.urls')),
-    path('library/', include('apps.library.urls')),
-    path('main/', include('apps.main.urls')),
-    path('news/', include('apps.news.urls')),
+    path("cart/", include("apps.cart.urls")),
+    path("common/", include("apps.common.urls")),
+    path("course/", include("apps.course.urls")),
+    path("feedback/", include("apps.feedback.urls")),
+    path("library/", include("apps.library.urls")),
+    path("main/", include("apps.main.urls")),
+    path("news/", include("apps.news.urls")),
     # path('users/', include('apps.users.urls')),
 ]

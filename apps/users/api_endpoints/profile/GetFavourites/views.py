d896a9b82487f0777e33f88eb7cfa2c853372e9b
fnabiyevuz <fnabiyevuz@gmail.com>
2023-03-28 10:39:08 +0500

    fix getfavourites

--- apps/users/api_endpoints/profile/GetFavourites/views.py
+++ apps/users/api_endpoints/profile/GetFavourites/views.py
@@ -1,15 +1,9 @@
 from rest_framework.generics import ListAPIView
 from rest_framework.permissions import IsAuthenticated
 
-<<<<<<< HEAD
-from apps.library.api_endpoints.book.BookList.serializers import \
-=======
-from apps.course.api_endpoints.course.CourseList.serializers import \
-    CourseListSerializer
+from apps.library.api_endpoints.book.BookList.serializers import BookSerializer as BookListSerializer
+from apps.course.api_endpoints.course.CourseList.serializers import CourseListSerializer
 from apps.course.models import Course
-from apps.library.api_endpoints.BookList.serializers import \
->>>>>>> eda245dc7df37f23f1318282038e4889cb81b6bb
-    BookSerializer as BookListSerializer
 from apps.library.models import Book
 
 
@@ -26,9 +20,6 @@ class GetFavouriteBooksAPIView(ListAPIView):
         return favourite_books
 
 
-<<<<<<< HEAD
-__all__ = ["GetFavouriteBooksAPIView"]
-=======
 class GetFavouriteCoursesAPIView(ListAPIView):
     serializer_class = CourseListSerializer
     permission_classes = [IsAuthenticated]
@@ -43,4 +34,3 @@ class GetFavouriteCoursesAPIView(ListAPIView):
 
 
 __all__ = ["GetFavouriteBooksAPIView", "GetFavouriteCoursesAPIView"]
->>>>>>> eda245dc7df37f23f1318282038e4889cb81b6bb
